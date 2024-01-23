6f2e004fc69f51dbc61e3bf0adfa4341befcfe13
Riya Shakya <riyashakya10@gmail.com>
2024-01-24 03:41:12 +0530

    created HTML structure with basic styling in CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {},
+    colors: {
+      "purple" : "#6A4C93",
+      "yellow" : "#FFCA3A",
+      "green" : "#8AC926",
+      "blue" : "#1982C4",
+      "pink" : "#FF595E",
+      "yellow-blur" : "rgba(255, 202, 58, 0.5)",
+      "green-blur" : "rgba(138, 201, 38, 0.5)"
+    }
   },
   plugins: [],
 }
