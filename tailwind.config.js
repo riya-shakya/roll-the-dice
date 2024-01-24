ef9910918b0f7e620ac99970fbf30c0c44422a55
Riya Shakya <riyashakya10@gmail.com>
2024-01-25 03:01:54 +0530

    solved the issue of extendibility of the default tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-      "purple" : "#6A4C93",
-      "yellow" : "#FFCA3A",
-      "green" : "#8AC926",
-      "blue" : "#1982C4",
-      "pink" : "#FF595E",
-      "yellow-blur" : "rgba(255, 202, 58, 0.5)",
-      "green-blur" : "rgba(138, 201, 38, 0.5)"
-    }
+    extend: {
+      colors: {
+        "purple" : "#6A4C93",
+        "yellow" : "#FFCA3A",
+        "green" : "#8AC926",
+        "blue" : "#1982C4",
+        "pink" : "#FF595E",
+        "yellow-blur" : "rgba(255, 202, 58, 0.5)",
+        "green-blur" : "rgba(138, 201, 38, 0.5)"
+      }
+    },
   },
   plugins: [],
 }
